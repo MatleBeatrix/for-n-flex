974773aa5cc4c97931037b5a4fd4adf5f0562b5e
Beatrix Mátlé <matlebeatrix@edu.bme.hu>
2021-11-17 15:39:49 +0100

    script null changed

--- script.js
+++ script.js
@@ -1,41 +1,32 @@
+function generateNull(maxLength, item){
+    let string = "";
+    
+    for (let i = 0; i < maxLength.toString().length - item.toString().length; i++) {
+             string += "0";
+    }
+    
+    return string;
+}
+
+
 const generateArray = function (amount){
     let returnArray = [];
+    amount = parseInt(amount);
 
-    /*
-    let amountConverted = parseInt(amount);
-    amount = amountConverted;
-    */
-   amount = parseInt(amount);
-   /*
-   console.log(amount);
-   console.log(typeof amount);
-    */
-
-   //Number.isNaN(amount) !== true
     if (!Number.isNaN(amount)) {
         for (let i = 0; i < amount; i++) {
-            returnArray.push(`${i}`);
-            //console.log(returnArray);
+            returnArray.push(`${generateNull(amount, i)}${i}`);
         }  
     } else {
         returnArray.push("error");
     }
-
-    //console.log(returnArray);
     return returnArray;
 };
 
 
 function loadEvent() {
-    //console.log(`Az oldal betöltödött.`);
-    //console.log(generateArray(100));
-    //console.log(generateArray("100"));
-    //console.log(generateArray([100]));
-    //console.log(generateArray("kiskutya"));
-    //console.log(generateArray("1 kiskutya"));
-
     const root = document.getElementById("root");
-    const list = generateArray(6);
+    const list = generateArray(1001);
     if (list[0] !== "error") {
         for (const item of list) {
             root.insertAdjacentHTML("beforeend",`<div>${item}</div>`);
@@ -44,21 +35,3 @@ function loadEvent() {
 }
 
 window.addEventListener("load", loadEvent);
-
-/*
-::::::::::::::::TOVÁBBI VÁLTOZATÓK:::::::::::::::
-
-window.addEventListener("load", function(){
-    ...
-};
-
-window.addEventListener("load",  () => {
-    ...
-};
-
-HA NINCS PARAMÉTER
-````````````````````
-window.addEventListener("load", _ => {
-    ...
-};
-*/
